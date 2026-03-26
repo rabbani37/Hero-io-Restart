d63adb077427f00362fd818fa5bc6b000b4a8caa
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-03-26 23:44:08 +0600

    change page loading

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,21 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "./NavBar/Navbar";
 import Footer from "./Footer/Footer";
-import { useContext, useEffect } from "react";
+
 import AppContext from "../contextApp/AppContextSepareted";
 import { Vortex } from "react-loader-spinner";
 
 const Root = () => {
 
-    const { spinner, setSpinner } = useContext(AppContext);
-
-    // useEffect(() => {
-        
-    //     setTimeout(() => {
-    //         setSpinner(false)
-    //     }, 400)
-
-    // }, [setSpinner])
+   
 
 
     return (
